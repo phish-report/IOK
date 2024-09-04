fff62b5eb59ffac8901f42c312376ce7704482f4
Bradley Kemp <bradley@bradleyjkemp.dev>
2024-09-04 15:24:52 +0100

    enable using new bundle evaluator

--- go.mod
+++ go.mod
@@ -1,16 +1,19 @@
 module phish.report/IOK
 
-go 1.17
+go 1.21
+
+toolchain go1.22.2
 
 require (
 	github.com/bradleyjkemp/cupaloy/v2 v2.6.0
-	github.com/bradleyjkemp/sigma-go v0.6.4
+	github.com/bradleyjkemp/sigma-go v0.6.6
 	golang.org/x/net v0.17.0
 	golang.org/x/sync v0.3.0
 	phish.report/urlscanio-go v0.0.0-20230915155435-2677d74fc8a2
 )
 
 require (
+	github.com/BobuSumisu/aho-corasick v1.0.3 // indirect
 	github.com/PaesslerAG/gval v1.0.0 // indirect
 	github.com/PaesslerAG/jsonpath v0.1.1 // indirect
 	github.com/alecthomas/participle v0.7.1 // indirect
--- iok.go
+++ iok.go
@@ -37,6 +37,19 @@ func GetMatches(input Input) ([]sigma.Rule, error) {
 	return GetMatchesForRules(input, evaluators)
 }
 
+func GetMatchesForRulesBundle(input Input, rules evaluator.RuleEvaluatorBundle) ([]sigma.Rule, error) {
+	matches := []sigma.Rule{}
+	ruleInput := convertInput(input)
+	results, err := rules.Matches(context.Background(), ruleInput)
+	for _, result := range results {
+		if result.Match {
+			matches = append(matches, result.Rule)
+		}
+	}
+
+	return matches, err
+}
+
 func GetMatchesForRules(input Input, rules []*evaluator.RuleEvaluator) ([]sigma.Rule, error) {
 	matches := []sigma.Rule{}
 	ruleInput := convertInput(input)
@@ -94,6 +107,14 @@ func ParseRule(path string, contents []byte) (*evaluator.RuleEvaluator, error) {
 	}
 
 	return evaluator.ForRule(rule, evaluator.WithConfig(config), evaluator.CaseSensitive), nil
+}
+
+func RulesBundle(rules []sigma.Rule) (evaluator.RuleEvaluatorBundle, error) {
+	config, err := sigma.ParseConfig(config)
+	if err != nil {
+		return evaluator.RuleEvaluatorBundle{}, fmt.Errorf("failed to parse config: %w", err)
+	}
+	return evaluator.ForRules(rules, evaluator.WithConfig(config), evaluator.CaseSensitive), nil
 }
 
 func init() {
